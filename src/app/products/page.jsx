ae8c935060e55983cd0f7e438b5b821fac7eb38b
agustin <agustinsanchezx@gmail.com>
2024-03-21 18:41:54 -0300

    agregando las funciones para sumar y restar cantidad en useContext

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,19 +1,16 @@
 'use client'
 import { useProducts } from '@/context/ProductsContext'
-import React, { useState } from 'react'
-import { products } from '@/api'
+import React from 'react'
 import Card from '@/components/Card'
 
 export default function Products() {
-
-    const { products } = useProducts()
-    console.log(products)
-
+    // Me traigo los productos del useContext
+    const { productsIphone } = useProducts()
 
     return (
         <>
             <div className='flex flex-col justify-center items-center gap-3 p-3'>
-                {products.map((product) => (
+                {productsIphone.map((product) => (
                     <Card 
                         product={product}
                         key={product.id}
