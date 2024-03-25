84e370684bc271c2cddf8d54d0bccfd434d40151
agustin <agustinsanchezx@gmail.com>
2024-03-25 17:25:50 -0300

    add input de busqueda y nuevos productos

--- src/api.js
+++ src/api.js
@@ -7,7 +7,8 @@ export const products = [
         descripcion: "iPhone con chip A13 Bionic, sistema de cámara dual de 12 MP, pantalla Liquid Retina HD de 6.1 pulgadas.",
         precio: 699,
         stock: 20,
-        cantidad: 1
+        cantidad: 1,
+        categoria: "celulares"
     },
     {
         id: 2,
@@ -16,7 +17,8 @@ export const products = [
         descripcion: "iPhone con diseño clásico y chip A14 Bionic, sistema de cámara dual avanzado y pantalla Super Retina XDR.",
         precio: 799,
         stock: 15,
-        cantidad: 1
+        cantidad: 1,
+        categoria: "celulares"
     },
     {
         id: 3,
@@ -25,7 +27,8 @@ export const products = [
         descripcion: "El último modelo de iPhone con pantalla Super Retina XDR, chip A15 Bionic y sistema de cámara dual avanzado.",
         precio: 999,
         stock: 25,
-        cantidad: 1
+        cantidad: 1,
+        categoria: "celulares"
     },
     {
         id: 4,
@@ -34,7 +37,8 @@ export const products = [
         descripcion: "iPhone con chip A16 Bionic, sistema de cámara triple de 16 MP, pantalla OLED de 6.7 pulgadas.",
         precio: 1199,
         stock: 18,
-        cantidad: 1
+        cantidad: 1,
+        categoria: "celulares"
     },
     {
         id: 5,
@@ -43,6 +47,107 @@ export const products = [
         descripcion: "iPhone con diseño futurista, chip A17 Bionic, sistema de cámara cuádruple y pantalla holográfica.",
         precio: 1399,
         stock: 22,
-        cantidad: 1
+        cantidad: 1,
+        categoria: "celulares"
+    },
+    {
+        id: 6,
+        titulo: "Teclado Mecánico RGB",
+        imagen: "/teclado_rgb.png",
+        descripcion: "Teclado mecánico con retroiluminación RGB, switches mecánicos, y diseño ergonómico.",
+        precio: 99,
+        stock: 15,
+        cantidad: 1,
+        categoria: "teclados"
+    },
+    {
+        id: 7,
+        titulo: "Teclado Inalámbrico Compacto",
+        imagen: "/teclado_inalambrico.png",
+        descripcion: "Teclado inalámbrico compacto con conexión Bluetooth, compatible con múltiples dispositivos.",
+        precio: 49,
+        stock: 10,
+        cantidad: 1,
+        categoria: "teclados"
+    },
+    {
+        id: 8,
+        titulo: "Teclado Ergonómico Dividido",
+        imagen: "/teclado_ergonomico.png",
+        descripcion: "Teclado dividido ergonómico con reposamuñecas acolchado, reduce la fatiga en largas sesiones de escritura.",
+        precio: 79,
+        stock: 8,
+        cantidad: 1,
+        categoria: "teclados"
+    },
+    {
+        id: 9,
+        titulo: "Teclado para Gaming",
+        imagen: "/teclado_gaming.png",
+        descripcion: "Teclado diseñado para gaming con teclas retroiluminadas, anti-ghosting y teclas programables.",
+        precio: 129,
+        stock: 12,
+        cantidad: 1,
+        categoria: "teclados"
+    },
+    {
+        id: 10,
+        titulo: "Teclado Táctil Inalámbrico",
+        imagen: "/teclado_tactil.png",
+        descripcion: "Teclado táctil inalámbrico con panel táctil integrado, ideal para controlar dispositivos multimedia.",
+        precio: 69,
+        stock: 7,
+        cantidad: 1,
+        categoria: "teclados"
+    },
+    {
+        id: 11,
+        titulo: "Mouse Ergonómico Vertical",
+        imagen: "/mouse_ergonomico.png",
+        descripcion: "Mouse ergonómico vertical que reduce la tensión en la muñeca y el antebrazo.",
+        precio: 39,
+        stock: 18,
+        cantidad: 1,
+        categoria: "mouse"
+    },
+    {
+        id: 12,
+        titulo: "Mouse Inalámbrico Silencioso",
+        imagen: "/mouse_inalambrico.png",
+        descripcion: "Mouse inalámbrico con tecnología silenciosa, ideal para entornos de trabajo tranquilos.",
+        precio: 29,
+        stock: 20,
+        cantidad: 1,
+        categoria: "mouse"
+    },
+    {
+        id: 13,
+        titulo: "Mouse para Gaming con Luces RGB",
+        imagen: "/mouse_gaming_rgb.png",
+        descripcion: "Mouse para gaming con diseño ergonómico y luces RGB personalizables.",
+        precio: 49,
+        stock: 15,
+        cantidad: 1,
+        categoria: "mouse"
+    },
+    {
+        id: 14,
+        titulo: "Mouse Trackball Inalámbrico",
+        imagen: "/mouse_trackball.png",
+        descripcion: "Mouse con tecnología de trackball, sin necesidad de moverlo físicamente, ideal para espacios reducidos.",
+        precio: 59,
+        stock: 10,
+        cantidad: 1,
+        categoria: "mouse"
+    },
+    {
+        id: 15,
+        titulo: "Mouse Vertical con Conexión USB-C",
+        imagen: "/mouse_vertical.png",
+        descripcion: "Mouse vertical con conexión USB-C, promueve una postura más natural de la mano y el brazo.",
+        precio: 45,
+        stock: 12,
+        cantidad: 1,
+        categoria: "mouse"
     }
 ];
--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,17 +1,54 @@
 'use client'
 import { useProducts } from '@/context/ProductsContext'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from '@/components/Card'
 
 export default function Products() {
     // Me traigo los productos del useContext
     const { productsIphone } = useProducts()
 
+    // Estados para manejar la busqueda
+    const [search, setSearch] = useState('')
+    const [filteredProducts, setFilteredProducts] = useState([])
+
+    // Funcion que maneja la busqueda
+    const handleChange = (e) => {
+        setSearch(e.target.value)
+        if (search.length > 2) {
+            const filterValues = productsIphone.filter((prod) => {
+                const productCategory = prod.categoria.toLowerCase()
+                const q = e.target.value.toLowerCase()
+                if (productCategory.includes(q)) {
+                    return true
+                }
+                return false
+            })
+            setFilteredProducts(filterValues)
+        }
+
+        if (search.length === 2) {
+            setFilteredProducts(productsIphone)
+        }
+    }
+
+    // UseEffect que me renderiza primero todos los productos
+    useEffect(() => {
+        setFilteredProducts(productsIphone)
+    }, [])
+
     return (
         <>
             <div className='flex flex-col justify-center items-center gap-3 p-3'>
-                {productsIphone.map((product) => (
-                    <Card 
+                <input
+                    type="text"
+                    className='border border-black rounded-md p-2 outline-none'
+                    placeholder='Teclados, mouse, celulares..'
+                    value={search}
+                    onChange={handleChange}
+                />
+
+                {filteredProducts.map((product) => (
+                    <Card
                         product={product}
                         key={product.id}
                     />
