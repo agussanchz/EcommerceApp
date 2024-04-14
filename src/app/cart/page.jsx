e102e4efe8416629526b77b82e8eec7785070fa9
agustin <agustinsanchezx@gmail.com>
2024-04-13 21:58:37 -0300

    nuevos estilos en card y separando boton finalizar compra en cart

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -12,8 +12,8 @@ export default function Page() {
   const handleClick = () => {
     setTimeout(() => {
       setCart([])
-    },1000);
-    
+    }, 1000);
+
     router.push('/products/sucess')
   }
 
@@ -21,35 +21,39 @@ export default function Page() {
     <div className='flex flex-wrap justify-center gap-3 p-3'>
       {cart.length === 0 ? (
         <>
-        <div className='flex flex-col justify-center gap-12 items-center h-screen'>
-          <h2 className='font-bold text-8xl text-slate-200 '>
-            Su carrito está vacío <span className='text-orange-400'>:(</span>
-          </h2>
-          <Link href={'/products'} className='font-bold bg-orange-400 rounded-md  p-3 text-slate-200 hover:border hover:border-orange-400 hover:bg-transparent'>
-            Descubrir productos
-          </Link>
-        </div>
+          <div className='flex flex-col justify-center gap-12 items-center h-screen'>
+            <h2 className='font-bold text-8xl text-slate-200 '>
+              Su carrito está vacío <span className='text-orange-400'>:(</span>
+            </h2>
+            <Link href={'/products'} className='font-bold bg-orange-400 rounded-md  p-3 text-slate-200 hover:border hover:border-orange-400 hover:bg-transparent'>
+              Descubrir productos
+            </Link>
+          </div>
         </>
       ) : (
-        cart.map((prod, index) => (
-          <div key={index} className='flex flex-col justify-between items-center p-4 border border-black rounded-xl text-slate-200 sm:w-72 md:w-96 bg-[#9ca3af16]'>
-            <Image
-              src={prod.imagen}
-              alt={`Imagen de ${prod.titulo}`}
-              width={200}
-              height={620}
-            />
-            <div className='flex flex-col justify-start gap-1'>
-              <h2 className='text-orange-400 font-bold'>{prod.titulo}</h2>
-              <p className='font-bold'>${prod.precio} US</p>
-              <p className='text-sm'>{prod.descripcion}</p>
-              <span>Cantidad: <span>{prod.cantidad}</span></span>
-              <span>Total de la compra: ${prod.cantidad * prod.precio}</span>
-              <button className='bg-orange-400 p-2 rounded-md' onClick={handleClick}>Finalizar Compra</button>
-              <button className='bg-orange-50 p-2 text-black rounded-md' onClick={() => handleDelete(prod.id)}>Eliminar</button>
-            </div>
+        <section className='flex flex-col items-center gap-3'>
+          <div className='flex flex-wrap gap-3'>
+            {cart.map((prod, index) => (
+              <div key={index} className='flex flex-col justify-between items-center p-4 border border-black rounded-xl text-slate-200 sm:w-72 md:w-96 bg-[#9ca3af16]'>
+                <Image
+                  src={prod.imagen}
+                  alt={`Imagen de ${prod.titulo}`}
+                  width={200}
+                  height={620}
+                />
+                <div className='flex flex-col justify-start gap-1'>
+                  <h2 className='text-orange-400 font-bold'>{prod.titulo}</h2>
+                  <p className='font-bold'>${prod.precio} US</p>
+                  <p className='text-sm'>{prod.descripcion}</p>
+                  <span>Cantidad: <span>{prod.cantidad}</span></span>
+                  <span>Total de la compra: ${prod.cantidad * prod.precio}</span>
+                  <button className='bg-orange-50 p-2 text-black rounded-md' onClick={() => handleDelete(prod.id)}>Eliminar</button>
+                </div>
+              </div>
+            ))}
           </div>
-        ))
+          <button className='bg-orange-400 p-2 rounded-md w-48' onClick={handleClick}>Finalizar Compra</button>
+        </section>
       )}
     </div>
   )
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,8 @@
 import Image from 'next/image'
 import React from 'react'
 import { useProducts } from '@/context/ProductsContext'
-
+import { FaArrowUp } from "react-icons/fa";
+import { FaArrowDown } from "react-icons/fa";
 
 export default function Card({ product }) {
     // Obtengo las funciones para restar y aumentar la cantidad de useContext
@@ -10,25 +11,34 @@ export default function Card({ product }) {
 
 
     return (
-        <div className='flex flex-col justify-between items-center p-4 border border-black rounded-xl text-slate-200 sm:w-72 md:w-96 bg-[#9ca3af16]  xl:hover:border-orange-400'>
+        <div className='flex flex-col justify-between items-center p-4 border border-black rounded-xl text-slate-200 shadow-sm shadow-[#585b61be] sm:w-72 md:w-96 bg-[#9ca3af16]'>
             <Image
                 src={product.imagen}
                 alt={`Imagen de ${product.titulo}`}
                 width='250'
                 height='250'
             />
-            <div className='flex flex-col p-4 justify-start gap-1'>
-                <h2 className='font-bold text-orange-400'>{product.titulo}</h2>
-                <p className='font-bold'>${product.precio} US</p>
-                <p className='text-sm'>{product.descripcion}</p>
-                <span>Cantidad:</span>
-                <div className='flex justify-center items-center gap-3'>
-                    <button className='border  w-8 rounded-md' onClick={() => handleRest(product.id)}>-</button>
-                    <span>{product.cantidad}</span>
-                    <button className='border  w-8 rounded-md' onClick={() => handleSum(product.id)}>+</button>
+            <div className='flex flex-col p-4 justify-start gap-4'>
+                <div className='flex flex-col gap-1'>
+                    <h2 className='font-bold text-orange-400'>{product.titulo}</h2>
+                    <p className='font-bold'>${product.precio} US</p>
+                    <p className='text-sm'>{product.descripcion}</p>
+                    
                 </div>
+                <div className='flex justify-center items-center gap-3'> 
+                        <span className='font-bold'>Cantidad:</span>
+                      
+                    <div className='flex items-center text-center gap-1 '>  
+                        <button className='bg-[#585b61be]  p-1 rounded-xl' onClick={() => handleRest(product.id)}><FaArrowDown /></button>
+                        <span className=' border-slate-200 w-5 text-center'>{product.cantidad}</span>
+                        <button className='bg-[#585b61be] p-1 rounded-xl' onClick={() => handleSum(product.id)}><FaArrowUp /></button>
+                    </div>
+                </div>
+                <div className='flex flex-col gap-1'>
+
                 <button className='bg-orange-400 p-2 text-slate-200 rounded-md' onClick={() => handleProductId(product.id)}>Comprar Ahora</button>
                 <button className='bg-orange-50 p-2 rounded-md text-black' onClick={() => handleCart(product.id)}>Agregar al carrito</button>
+                </div>
             </div>
         </div>
     )
