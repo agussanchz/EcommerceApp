d66269059c94969a41827a635d70be16008dfcbb
agustin <agustinsanchezx@gmail.com>
2024-04-15 17:33:36 -0300

    cambiando tamaño de modal

--- src/components/ModalCart.jsx
+++ src/components/ModalCart.jsx
@@ -7,7 +7,7 @@ export default function ModalCart() {
     return (
         showModal === true ? (
             <div className='fixed inset-0 flex justify-center items-center bg-[#22232416]'>
-                <div className='w-96 h-80 p-3 flex flex-col gap-4 justify-center items-center  bg-[#0101016d] rounded-2xl'>
+                <div className='w-80 h-80 p-3 flex flex-col gap-4 justify-center items-center  bg-[#0101016d] rounded-2xl'>
                     <h2 className='text-5xl font-bold'>Producto agregado al carrito</h2>
                     <button className='font-bold bg-orange-400 rounded-md w-32 p-3 text-slate-200 hover:border hover:border-orange-400 hover:bg-transparent ' onClick={() => setShowModal(false)}>Cerrar</button>
                 </div>
